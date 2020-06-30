4107b9f219589abc36b232fbe7ae6f470b613ae7
Weijian Rao <weijianrao@gmail.com>
2020-07-01 00:56:29 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install zsh python3-pip ctags cmake
+sudo apt install zsh python3-pip ctags cmake curl
 sudo pip3 install flake8
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp ./.zshrc ~/
